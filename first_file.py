4b82ac6817cf16c4b984d8434a5bd8cc4cca2f4b
GorgeousGazelle <acho0111@student.monash.edu>
2024-10-10 20:24:14 +0800

    aded new data to disease_ages

--- first_file.py
+++ first_file.py
@@ -1,1 +1,5 @@
-print("Hello World!")
+import pandas as pd  
+df = pd.read_csv("FIT3179-Assignment-2\Disease_ages.csv")
+print(df)
+df["normalised_incidence"] = df["total_occurrence"] / df["population"]
+df.to_csv('FIT3179-Assignment-2\Disease_ages.csv', index=False)
